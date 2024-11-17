16943c42dcb9d2efa93192dfdbe65314d525ba6b
yudhi66 <yudhivikram536@gmail.com>
2024-11-18 01:31:59 +0530

    cloudinary

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
      statusCode,
      message="Something went wrong",
    errors=[],
-   statck="" 
+   stack="" 
   
     ){
 
@@ -14,8 +14,8 @@ class ApiError extends Error{
    this.success =false
    this.errors= errors
     
-   if(statck){
-    this.stack=statck
+   if(stack){
+    this.stack=stack
    }else{
     Error.captureStackTrace(this,this.constructor)
    }
