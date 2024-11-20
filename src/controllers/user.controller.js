1bfe03d12c56b8b33ede8dbfb256e4a6bc068fa3
yudhi66 <yudhivikram536@gmail.com>
2024-11-21 01:53:01 +0530

    register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,36 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+
 
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+
+
+  const {fullName,email,username,password} = req.body
+
+  console.log(`email ${email}`);
+
+  if(
+    [fullName,email,username,password].some(()=>
+    filed?.trim()===""
+    )
+  ){
+    throw new ApiError(400,"All fields are required ")
+
+  }
+
+const existeduser =  User.findOne({
+   $or:[{username},{email}]
+
+  })
+
+  if(existeduser){
+    throw new ApiError(409,"User with email or username already exists")
+  }
+
+  req>files?.avatar[0]?.path
+ 
 })
 
 
