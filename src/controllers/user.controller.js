90ea9dd01551823bf1f867689675dc5d3985ef2b
yudhi66 <yudhivikram536@gmail.com>
2024-11-24 01:05:25 +0530

    jwt verification

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -149,7 +149,24 @@ const loginUser =asyncHandler(async (req,res)=>{
 })
 
 const logoutUser =asyncHandler(async(req,res)=>{
-  User.findById
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },{
+      new:true
+    }
+   )
+   const options={
+    httpOnly:true,
+    secure:true
+   }
+
+
+   return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options)
+   .json(new ApiResponse(200,{},"User logged Out"))
 
 })
 
@@ -157,4 +174,4 @@ const logoutUser =asyncHandler(async(req,res)=>{
 
 
 
-export {registerUser}
+export {registerUser,loginUser,logoutUser}
